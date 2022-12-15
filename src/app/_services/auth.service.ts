78d782a58ce53ccbc58a2527554440688ce2a15f
OSCAR BLANCO <nicolasblanco201008@gmail.com>
2022-12-14 20:17:57 -0500

    PQRS terminado

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,23 +1,36 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { TokenStorageService } from './token-storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  header = new HttpHeaders({
+    token: this.tokenservice.getToken()!,
+  });
 
-  signup(account:any) {
+  constructor(private http: HttpClient, private tokenservice: TokenStorageService) { }
+
+  signup(account: any) {
     console.log("En el signUp");
     console.log(environment.apiUrl);
-    return this.http.post(`${environment.apiUrl}/api/accounts/newAccount`,account)
-    
-  }
-  login(account:any){
-    console.log("En el login Service");
-    return this.http.post(`${environment.apiUrl}/api/auth/login`,account)
+    return this.http.post(`${environment.apiUrl}/api/accounts/newAccount`, account)
 
+  }
+  login(account: any) {
+    console.log("En el login Service");
+    console.log(environment.apiUrl);
+    return this.http.post(`${environment.apiUrl}/api/auth/login`, account)
+  }
+
+
+  savePqrs(account: any) {
+    console.log('En el pqrs');
+    console.log(environment.apiUrl);
+    return this.http.post(`${environment.apiUrl}/api/regPQRS/pqrs`, account ,{ headers: this.header })
   }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,6 +21,11 @@ const routes: Routes = [{
       loadChildren: () =>
         import('./profile/profile.module').then((m) => m.ProfileModule),
     },
+    {
+      path: 'pqrs',
+      loadChildren: () =>
+        import('./pqrs/pqrs.module').then((m) => m.PqrsModule),
+    },
   ]
 },
 {
